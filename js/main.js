f1899f9f01af483c4a33a8ea032535b698f50166
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2021-02-10 02:30:36 +0200

    remove gsap

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-$(".home-banner").parallax({ imageSrc: "../assets/home-banner.png" });
-AOS.init();
-gsap.to(".about-text", {
-  scrollTrigger: {
-    trigger: ".about-text",
-    start: "-300px 500px",
-    end: "600px",
-    scrub: true, 
-  },
-  y: 100,
-  duration: 3,
-  delay: 0.2,
-  ease: "power1.inOut",
-});
+$(".home-banner").parallax({ imageSrc: "../assets/img/home-banner.png" });
+
+// gsap.to(".about-text", {
+//   scrollTrigger: {
+//     trigger: ".about-text",
+//     start: "-300px 500px",
+//     end: "600px",
+//     scrub: true, 
+//   },
+//   y: 100,
+//   duration: 3,
+//   delay: 0.2,
+//   ease: "power1.inOut",
+// });
