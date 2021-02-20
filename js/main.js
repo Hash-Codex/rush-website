ae290ab729a7dd2396cc123894a55c0a58e655bc
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2021-02-21 01:28:08 +0200

    temp push

--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@ $('#exampleModal3').on('shown.bs.modal', function () {
   $('#exampleModal3').on('hidden.bs.modal', function () {
     $('#video1')[0].pause();
   })
+  function vc(id) {
+    $(id).get(0).pause()
+  }
